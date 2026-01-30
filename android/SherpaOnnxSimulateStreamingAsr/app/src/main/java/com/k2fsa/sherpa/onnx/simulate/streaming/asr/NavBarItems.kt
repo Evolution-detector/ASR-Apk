df5739be67fb5977b5be5d35ede72d54a79eeea0
Evolution-detector <64944480+Evolution-detector@users.noreply.github.com>
2026-01-30 02:46:16 +0000

    Refactor NavBarItems to use titleResId

--- android/SherpaOnnxSimulateStreamingAsr/app/src/main/java/com/k2fsa/sherpa/onnx/simulate/streaming/asr/NavBarItems.kt
+++ android/SherpaOnnxSimulateStreamingAsr/app/src/main/java/com/k2fsa/sherpa/onnx/simulate/streaming/asr/NavBarItems.kt
@@ -3,17 +3,16 @@ package com.k2fsa.sherpa.onnx.simulate.streaming.asr
 import androidx.compose.material.icons.Icons  
 import androidx.compose.material.icons.filled.Home  
 import androidx.compose.material.icons.filled.Info  
-import androidx.compose.ui.res.stringResource  
   
 object NavBarItems {  
     val BarItems = listOf(  
         BarItem(  
-            title = stringResource(R.string.home),   
+            titleResId = R.string.home,  
             image = Icons.Filled.Home,  
             route = "home",  
         ),  
         BarItem(  
-            title = stringResource(R.string.help),   
+            titleResId = R.string.help,  
             image = Icons.Filled.Info,  
             route = "help",  
         ),  
